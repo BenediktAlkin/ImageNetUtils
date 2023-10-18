8d862026792c299bb41a37905f77bb6f3c14cbcf
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-10-19 01:11:22 +0200

    versions

--- imagenet_utils/versions/__init__.py
+++ imagenet_utils/versions/__init__.py
@@ -1,3 +1,5 @@
 from .in1k import CLASSES as IN1K_CLASSES
+from .in10_clustering import CLASSES as IN10_CLUSTERING_CLASSES
 from .in13_felines import CLASSES as IN13_FELINES_CLASSES
+from .in15_dogs import CLASSES as IN15_DOGS_CLASSES
 from .in118_dogs import CLASSES as IN118_DOGS_CLASSES
